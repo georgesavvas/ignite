5c3cca2ef9320faaac61a9cb6477dbee4105492b
George <georgesavvas@users.noreply.github.com>
2022-03-24 13:18:27 +0000

    wip

---
+++ ui/src/components/Browser.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState, useContext} from "react";
+import styles from "./AssetTile.module.css";
+
+export default function AssetTile() {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+}
