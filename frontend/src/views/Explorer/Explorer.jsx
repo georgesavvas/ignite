3de499c32e6189fb97527a04f5386887a28ea86d
George <georgesavvas@users.noreply.github.com>
2022-08-15 17:47:49 +0100

    wip

--- frontend/src/views/Explorer/Explorer.jsx
+++ frontend/src/views/Explorer/Explorer.jsx
@@ -20,8 +20,29 @@ import { ConfigContext } from "../../contexts/ConfigContext";
 
 const debounced = debounce(fn => fn(), 500);
 
-const defaultViewType = "grid";
-const defaultTileSize = 5;
+const defaultExplorerSettings = {
+  currentResultType: "dynamic",
+  currentViewType: "grid",
+  currentTileSize: 5,
+  tilesPerPage: 50,
+  saved: {
+    dynamic: {
+      grid: 5,
+      row: 5,
+      current: "grid"
+    },
+    assets: {
+      grid: 5,
+      row: 5,
+      current: "grid"
+    },
+    scenes: {
+      grid: 5,
+      row: 5,
+      current: "grid"
+    }
+  }
+}
 
 function Explorer() {
   const [config, setConfig] = useContext(ConfigContext);
@@ -30,11 +51,7 @@ function Explorer() {
   const [loadedData, setLoadedData] = useState([]);
   const [pages, setPages] = useState({total: 1, current: 1});
   const [query, setQuery] = useState({latest: 1});
-  const [explorerSettings, setExplorerSettings] = useState("dynamic");
-  const [tileSize, setTileSize] = useState(defaultTileSize);
-  const [tilesPerPage, setTilesPerPage] = useState(50);
-  const [resultType, setResultType] = useState("dynamic");
-  const [viewType, setViewType] = useState(defaultViewType);
+  const [explorerSettings, setExplorerSettings] = useState(defaultExplorerSettings);
   const [tiles, setTiles] = useState([]);
   const [modalData, setModalData] = useState({});
   const [selectedEntity, setSelectedEntity] = useContext(EntityContext);
@@ -59,29 +76,21 @@ function Explorer() {
   useEffect(() => {
     const data = loadExplorerSettings();
     if (!data) return;
-    setTilesPerPage(data.tilesPerPage || 50);
-    setViewType(data.viewType[resultType] || defaultViewType);
-    setTileSize(data.tileSize[resultType][viewType[resultType]] || defaultTileSize);
+    setExplorerSettings(data);
   }, []);
 
   useEffect(() => {
-    const data = {
-      resultType: resultType,
-      viewType: viewType,
-      tileSize: tileSize
-    }
-    const settings = saveExplorerSettings(data);
-    setExplorerSettings(settings);
-  }, [resultType, viewType, tileSize])
+    saveExplorerSettings(explorerSettings);
+  }, [explorerSettings])
 
   useEffect(() => {
     const data = {
       page: pages.current,
-      limit: tilesPerPage,
+      limit: explorerSettings.tilesPerPage,
       path: BuildFileURL(currentContext.path, config, {reverse: true, pathOnly: true}),
       query: query
     };
-    const method = methods[resultType];
+    const method = methods[explorerSettings.currentResultType];
     setIsLoading(true);
     if (!Object.entries(config.access).length) return;
     serverRequest(method, data).then(resp => {
@@ -89,7 +98,7 @@ function Explorer() {
       setLoadedData(resp.data);
       setPages((prevPages) => ({...prevPages, total: resp.pages.total}));
     });
-  }, [pages.current, resultType, refreshValue, currentContext, config.access, tilesPerPage, query]);
+  }, [pages.current, explorerSettings.currentResultType, explorerSettings.tilesPerPage, refreshValue, currentContext, config.access, query]);
 
   useEffect(() => {
     const _tiles = loadedData.reduce(function(obj, entity) {
@@ -103,13 +112,13 @@ function Explorer() {
       if (entity.scene) entity.scene = BuildFileURL(entity.scene, config, {pathOnly: true});
       if (entity.dir_kind === "assetversion") {
         obj[entity.result_id] = <AssetTile key={entity.result_id} entity={entity}
-          onSelected={handleEntitySelection} size={tileSize * 40} viewType={viewType}
+          onSelected={handleEntitySelection} size={explorerSettings.currentTileSize * 40} viewType={explorerSettings.currentViewType}
           selected={selectedEntity.path === entity.path} refreshContext={refreshContext}
           onContextMenu={handleContextMenuSelection}
         />;
       } else {
         obj[entity.result_id] = <DirectoryTile key={entity.result_id} entity={entity}
-          onSelected={handleEntitySelection} size={tileSize * 40} viewType={viewType}
+          onSelected={handleEntitySelection} size={explorerSettings.currentTileSize * 40} viewType={explorerSettings.currentViewType}
           selected={selectedEntity.path === entity.path} refreshContext={refreshContext}
           onContextMenu={handleContextMenuSelection}
         />;
@@ -117,7 +126,7 @@ function Explorer() {
       return obj;
     }, {});
     setTiles(_tiles);
-  }, [loadedData, selectedEntity, viewType, tileSize])
+  }, [loadedData, selectedEntity, explorerSettings.currentViewType, explorerSettings.currentTileSize])
 
   const forceUpdate = () => {
     setRefreshValue((prevRefresh) => (prevRefresh + 1))
@@ -132,20 +141,42 @@ function Explorer() {
     debounced(() => setQuery((prevState) => ({...prevState, filter_string: value})));
   }
 
-  const handleTilesPerPageChange = e => {
-    setTilesPerPage(parseInt(e.target.value));
+  const handleTilesPerPageChange = value => {
+    setExplorerSettings(prevState => {
+      prevState.tilesPerPage = value;
+    })
   }
 
   const handleResultTypeChange = value => {
-    setResultType(value);
-    const view = explorerSettings.viewType[value];
-    setViewType(view || defaultViewType);
-    setTileSize(explorerSettings.tileSize[value][view] || defaultTileSize);
+    const savedViewType = explorerSettings.saved[value].current;
+    const savedTileSize = explorerSettings.saved[value][savedViewType];
+    setExplorerSettings(prevState => {
+      prevState.currentResultType = value;
+      prevState.currentViewType = savedViewType;
+      prevState.currentTileSize = savedTileSize;
+      return prevState;
+    })
   }
 
   const handleViewTypeChange = value => {
-    setViewType(value || defaultViewType);
-    setTileSize(explorerSettings.tileSize[resultType][value] || defaultTileSize);
+    const currentResultType = explorerSettings.currentResultType;
+    const savedTileSize = explorerSettings.saved[currentResultType][value];
+    setExplorerSettings(prevState => {
+      prevState.currentViewType = value;
+      prevState.currentTileSize = savedTileSize;
+      prevState.saved[currentResultType].current = value;
+      return prevState;
+    })
+  }
+
+  const handleTileSizeChange = value => {
+    const currentResultType = explorerSettings.currentResultType;
+    const currentViewType = explorerSettings.currentViewType;
+    setExplorerSettings(prevState => {
+      prevState.currentTileSize = value;
+      prevState.saved[currentResultType][currentViewType] = value;
+      return prevState;
+    })
   }
 
   const handleLatestChange = e => {
@@ -156,13 +187,13 @@ function Explorer() {
     flexGrow: 1,
     display: "grid",
     overflowY: "auto",
-    gridTemplateColumns: `repeat(auto-fill, minmax(${tileSize * 40}px, 1fr))`,
+    gridTemplateColumns: `repeat(auto-fill, minmax(${explorerSettings.currentTileSize * 40}px, 1fr))`,
     gridGap: "10px",
     padding: "10px",
     transition: "0.2s"
   }
 
-  if (viewType === "row") {
+  if (explorerSettings.currentViewType === "row") {
     tileContainerStyle.gridTemplateColumns = `repeat(1, 1fr)`;
   }
 
@@ -183,9 +214,9 @@ function Explorer() {
       <ExplorerBar
         onRefresh={forceUpdate}
         onFilterChange={handleFilterChange}
-        resultType={resultType}
+        resultType={explorerSettings.currentResultType}
         onResultTypeChange={handleResultTypeChange}
-        viewType={viewType}
+        viewType={explorerSettings.currentViewType}
         onLatestChange={handleLatestChange}
         onViewTypeChange={handleViewTypeChange}
         enqueueSnackbar={enqueueSnackbar}
@@ -197,7 +228,7 @@ function Explorer() {
       </div>
       <div className={classes.layoutHelper} />
       <Divider />
-      <PageBar pages={pages.total} onChange={handlePageChange} tileSize={tileSize} onTilesPerPageChange={handleTilesPerPageChange} onTileSizeChange={e => setTileSize(e.target.value)}/>
+      <PageBar pages={pages.total} onChange={handlePageChange} tileSize={explorerSettings.currentTileSize} onTilesPerPageChange={e => handleTilesPerPageChange(e.target.value)} onTileSizeChange={e => handleTileSizeChange(e.target.value)}/>
     </div>
   )
 }
