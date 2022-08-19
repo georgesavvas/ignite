f9d052112da89c3add5cc7085da81c0c3acb7cdd
George <georgesavvas@users.noreply.github.com>
2022-08-19 10:11:41 +0100

    wip

--- frontend/src/views/DetailsView/Attributes.jsx
+++ frontend/src/views/DetailsView/Attributes.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useRef, useMemo } from "react";
 import styles from "./Attributes.module.css";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
@@ -7,42 +7,69 @@ import { useSnackbar } from "notistack";
 import { CopyToClipboard } from "../ContextActions";
 import ContextMenu, { handleContextMenu } from "../../components/ContextMenu";
 import clientRequest from "../../services/clientRequest";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid } from '@mui/x-data-grid';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const columns = [
   { field: "name", headerName: "Name", editable: true, flex: 1 },
   { field: "inherited", headerName: "Inherited", editable: true, flex: 1 },
-  { field: "override", headerName: "Override", editable: true, flex: 1, editable: true }
+  { field: "override", headerName: "Override", editable: true, flex: 1 }
 ];
 
-const rows = [
-  {
-    id: 1,
-    name: "fstart",
-    value: 1001
-  },
-  {
-    id: 2,
-    name: "fend",
-    value: 1100
-  },
-  {
-    id: 3,
-    name: "fps",
-    value: 25
-  },
-  {id: 4}
-]
-
 function Attributes(props) {
+
+  function useApiRef() {
+    const apiRef = useRef(null);
+    const _columns = useMemo(
+      () =>
+        columns.concat({
+          field: "__HIDDEN__",
+          width: 0,
+          renderCell: (params) => {
+            apiRef.current = params.api;
+            return null;
+          }
+        }),
+      [columns]
+    );
+  
+    return { apiRef, columns: _columns };
+  }
+
+  const handleAddAttrib = e => {
+    const value = e.target.value;
+    apiRef.current.startCellEditMode({ id: 1, field: "name" });
+  }
+
   return (
     <div className={styles.container}>
-      <Typography variant="h5" style={{marginBottom: "10px"}}>Attributes</Typography>
+      <div className={styles.topBar}>
+        <Typography variant="h5" style={{marginBottom: "10px"}}>Attributes</Typography>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <Select
+            value="add"
+            onChange={handleAddAttrib}
+            inputProps={{ "aria-label": "Without label" }}
+            size="small"
+          >
+            <MenuItem disabled value="add">Add</MenuItem>
+            <MenuItem value="fstart">Start Frame</MenuItem>
+            <MenuItem value="fend">End Frame</MenuItem>
+            <MenuItem value="fps">FPS</MenuItem>
+            <MenuItem value="custom">Custom</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
       <div className={styles.attributeList}>
         <DataGrid
           disableSelectionOnClick
           rows={rows}
           columns={columns}
+          isCellEditable={params => params.row.inherited === ""}
           experimentalFeatures={{ newEditingApi: true }}
         />
       </div>
