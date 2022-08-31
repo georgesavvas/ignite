3da1828e0b4be7936ebb875ceb0c22f183b9d131
George <georgesavvas@users.noreply.github.com>
2022-08-31 23:49:38 +0100

    wip

---
+++ backend/src/bin/win/huey.bat
@@ -0,0 +1,3 @@
+title "IGNITE WORKER"
+%0\..\..\..\..\env\Scripts\activate && python %0\..\..\..\python\huey_consumer ignite_client.huey.HUEY -S
+PAUSE
--- backend/src/bin/win/server.bat
+++ backend/src/bin/win/server.bat
@@ -1,3 +1,4 @@
 git pull --ff-only
+title "IGNITE SERVER"
 %0\..\..\..\..\env\Scripts\activate && python %0\..\..\..\python\server_main.py
 PAUSE
