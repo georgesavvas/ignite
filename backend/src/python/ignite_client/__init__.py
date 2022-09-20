b4f327f5dee4e69633927731036f79fcc44a8568
George <georgesavvas@users.noreply.github.com>
2022-09-20 04:08:04 +0100

    wip

--- backend/src/python/ignite_client/__init__.py
+++ backend/src/python/ignite_client/__init__.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from platformdirs import user_config_dir, site_config_dir
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 DIR = os.path.dirname(__file__)
@@ -21,17 +21,17 @@ else:
     OS_NAME = "linux"
 
 api_v = "v1"
-logging.info(f"Setting IGNITE_API_VERSION to {api_v}")
+logging.debug(f"Setting IGNITE_API_VERSION to {api_v}")
 ENV["IGNITE_API_VERSION"] = api_v
 
 ignite_root = Path(DIR).parent.parent.parent.parent
-logging.info(f"Setting IGNITE_ROOT to {ignite_root}")
+logging.debug(f"Setting IGNITE_ROOT to {ignite_root}")
 ENV["IGNITE_ROOT"] = str(ignite_root)
 
 USER_CONFIG_PATH = Path.home() / ".ignite"
 if not USER_CONFIG_PATH.exists():
     USER_CONFIG_PATH.mkdir(parents=True, exist_ok=True)
-logging.info(f"Setting IGNITE_USER_CONFIG_PATH to {USER_CONFIG_PATH}")
+logging.debug(f"Setting IGNITE_USER_CONFIG_PATH to {USER_CONFIG_PATH}")
 ENV["IGNITE_USER_CONFIG_PATH"] = str(USER_CONFIG_PATH)
 
 def ensure_config(filepath, default={}):
@@ -68,23 +68,23 @@ ensure_config(CLIENT_CONFIG_PATH, {
         "server_projects_root": default_project_dir
     }
 })
-logging.info(f"Setting IGNITE_CLIENT_CONFIG_PATH to {CLIENT_CONFIG_PATH}")
+logging.debug(f"Setting IGNITE_CLIENT_CONFIG_PATH to {CLIENT_CONFIG_PATH}")
 ENV["IGNITE_CLIENT_CONFIG_PATH"] = str(CLIENT_CONFIG_PATH)
 
 CONFIG_PATH = ignite_root / "config"
-logging.info(f"Setting IGNITE_CONFIG_PATH to {CONFIG_PATH}")
+logging.debug(f"Setting IGNITE_CONFIG_PATH to {CONFIG_PATH}")
 ENV["IGNITE_CONFIG_PATH"] = str(CONFIG_PATH)
 
 dcc = CONFIG_PATH / "dcc"
-logging.info(f"Setting IGNITE_DCC to {dcc}")
+logging.debug(f"Setting IGNITE_DCC to {dcc}")
 ENV["IGNITE_DCC"] = str(dcc)
 
 tools = ignite_root / "tools" / OS_NAME
-logging.info(f"Setting IGNITE_TOOLS to {tools}")
+logging.debug(f"Setting IGNITE_TOOLS to {tools}")
 ENV["IGNITE_TOOLS"] = str(tools)
 
 ocio = dcc / "ocio/aces_1.2/config.ocio"
-logging.info(f"Setting OCIO to {ocio}")
+logging.debug(f"Setting OCIO to {ocio}")
 ENV["OCIO"] = str(ocio)
 
 houdini_modules = str(dcc / "houdini/python")
