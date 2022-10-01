db4ce2a155f290f2665f4cbce00b130a962bbaa0
George <georgesavvas@users.noreply.github.com>
2022-10-01 11:12:22 +0100

    wip

--- backend/src/bin/win/build_backend.bat
+++ backend/src/bin/win/build_backend.bat
@@ -1,2 +1,3 @@
+cd %0\..\..\..\.. && ^
 pyinstaller src/python/ignite/server_main.py --noconfirm --onefile --name=IgniteServer --icon ../frontend/public/media/desktop_icon/win/icon.ico
 pyinstaller src/python/ignite/client_main.py --noconfirm --onefile --name=IgniteClientBackend --icon ../frontend/public/media/desktop_icon/win/icon.ico
