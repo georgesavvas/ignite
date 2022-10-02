17cd6658279009ee77a527951d1b354e3f425e7c
George <georgesavvas@users.noreply.github.com>
2022-10-02 12:19:27 +0100

    wip

--- start_dev_win.bat
+++ start_dev_win.bat
@@ -1,8 +1,8 @@
-cd backend
+cd %0\backend
 echo %cd%
 wt.exe -w 0 nt -d . --tabColor "#1d34e0" .\src\bin\win\server.bat
 wt.exe -w 0 nt -d . --tabColor "#b6e01d" .\src\bin\win\client.bat
-cd ..\frontend
+cd %0\frontend
 echo %cd%
 wt.exe -w 0 nt -d . --tabColor "#8f1111" .\bin\win\electron.bat
 wt.exe -w 0 nt -d . --tabColor "#138017" .\bin\win\node.bat
