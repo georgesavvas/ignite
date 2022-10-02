cd30093b532f0edb0f26b59a98a465bb71a60420
George <georgesavvas@users.noreply.github.com>
2022-10-02 13:23:56 +0100

    added apache-2.0 license

--- backend/src/python/ignite/server_main.py
+++ backend/src/python/ignite/server_main.py
@@ -1,20 +1,29 @@
+# Copyright 2022 George Savvas
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import logging
 import math
 import os
 import sys
 from pathlib import Path
 
-import sentry_sdk
 import uvicorn
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
 from fastapi.staticfiles import StaticFiles
-
-sentry_sdk.init(
-    dsn="https://9930a18d142b45af9d27e35276e3de54@o1421552.ingest.sentry.io/6767422",
-    traces_sample_rate=1.0
-)
 
 from ignite.server import utils
 from ignite.vault import api as vault_api
