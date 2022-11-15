2b30e73220b55dceaaaf3ad492dc0d947e91e8d7
George <georgesavvas@users.noreply.github.com>
2022-11-15 13:22:34 +0000

    wip

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "Ignite",
   tagline: "Open Source VFX Pipeline",
-  url: "docs.ignitevfx.co.uk",
+  url: "https://docs.ignitevfx.co.uk",
   baseUrl: '/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
