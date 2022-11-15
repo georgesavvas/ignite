4df91e30c2dd3d1ad581e8bcb6e581b47375914f
George <cgeorgesavvas@gmail.com>
2022-11-15 02:16:39 +0000

    Docs (#3)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    Co-authored-by: George Savvas <george@mac.local>
    Co-authored-by: George <georgesavvas@users.noreply.github.com>

--- docs/.docusaurus/registry.js
+++ docs/.docusaurus/registry.js
@@ -1,4 +1,19 @@
 export default {
+<<<<<<< HEAD
+  '17896441': [() => import(/* webpackChunkName: '17896441' */ '@theme/DocItem'), '@theme/DocItem', require.resolveWeak('@theme/DocItem')],
+  '1be78505': [() => import(/* webpackChunkName: '1be78505' */ '@theme/DocPage'), '@theme/DocPage', require.resolveWeak('@theme/DocPage')],
+  '3b8c55ea': [() => import(/* webpackChunkName: '3b8c55ea' */ '@site/docs/installation.md'), '@site/docs/installation.md', require.resolveWeak('@site/docs/installation.md')],
+  '4ead559e': [() => import(/* webpackChunkName: '4ead559e' */ 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-pages\\default\\plugin-route-context-module-100.json'), 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-pages\\default\\plugin-route-context-module-100.json', require.resolveWeak('C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-pages\\default\\plugin-route-context-module-100.json')],
+  '5e9f5e1a': [() => import(/* webpackChunkName: '5e9f5e1a' */ '@generated/docusaurus.config'), '@generated/docusaurus.config', require.resolveWeak('@generated/docusaurus.config')],
+  '74a803a9': [() => import(/* webpackChunkName: '74a803a9' */ '@site/docs/basic_concepts.md'), '@site/docs/basic_concepts.md', require.resolveWeak('@site/docs/basic_concepts.md')],
+  '92bb876c': [() => import(/* webpackChunkName: '92bb876c' */ '@site/docs/getting_started.md'), '@site/docs/getting_started.md', require.resolveWeak('@site/docs/getting_started.md')],
+  '935f2afb': [() => import(/* webpackChunkName: '935f2afb' */ '~docs/default/version-current-metadata-prop-751.json'), '~docs/default/version-current-metadata-prop-751.json', require.resolveWeak('~docs/default/version-current-metadata-prop-751.json')],
+  'a3cbdead': [() => import(/* webpackChunkName: 'a3cbdead' */ '@site/docs/Developer/rest_api.md'), '@site/docs/Developer/rest_api.md', require.resolveWeak('@site/docs/Developer/rest_api.md')],
+  'c4f5d8e4': [() => import(/* webpackChunkName: 'c4f5d8e4' */ '@site/src/pages/index.js'), '@site/src/pages/index.js', require.resolveWeak('@site/src/pages/index.js')],
+  'd25f097e': [() => import(/* webpackChunkName: 'd25f097e' */ 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json'), 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json', require.resolveWeak('C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json')],
+  'e747ec83': [() => import(/* webpackChunkName: 'e747ec83' */ '@site/docs/glossary.md'), '@site/docs/glossary.md', require.resolveWeak('@site/docs/glossary.md')],
+  'f56d8649': [() => import(/* webpackChunkName: 'f56d8649' */ '@site/docs/Developer/python_api.md'), '@site/docs/Developer/python_api.md', require.resolveWeak('@site/docs/Developer/python_api.md')],};
+=======
   '__comp---site-src-pages-index-jsc-4-f-f99': [() => import(/* webpackChunkName: '__comp---site-src-pages-index-jsc-4-f-f99' */ '@site/src/pages/index.js'), '@site/src/pages/index.js', require.resolveWeak('@site/src/pages/index.js')],
   '__comp---theme-debug-config-23-a-2ff': [() => import(/* webpackChunkName: '__comp---theme-debug-config-23-a-2ff' */ '@theme/DebugConfig'), '@theme/DebugConfig', require.resolveWeak('@theme/DebugConfig')],
   '__comp---theme-debug-contentba-8-ce7': [() => import(/* webpackChunkName: '__comp---theme-debug-contentba-8-ce7' */ '@theme/DebugContent'), '@theme/DebugContent', require.resolveWeak('@theme/DebugContent')],
@@ -20,3 +35,4 @@ export default {
   'plugin---docsd-25-795': [() => import(/* webpackChunkName: 'plugin---docsd-25-795' */ 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json'), 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json', require.resolveWeak('C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-content-docs\\default\\plugin-route-context-module-100.json')],
   'plugin---docusaurus-debug-310-56f': [() => import(/* webpackChunkName: 'plugin---docusaurus-debug-310-56f' */ 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-debug\\default\\plugin-route-context-module-100.json'), 'C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-debug\\default\\plugin-route-context-module-100.json', require.resolveWeak('C:\\dev\\ignite\\docs\\.docusaurus\\docusaurus-plugin-debug\\default\\plugin-route-context-module-100.json')],
   'versionMetadata---docs-935-398': [() => import(/* webpackChunkName: 'versionMetadata---docs-935-398' */ '~docs/default/version-current-metadata-prop-751.json'), '~docs/default/version-current-metadata-prop-751.json', require.resolveWeak('~docs/default/version-current-metadata-prop-751.json')],};
+>>>>>>> main
