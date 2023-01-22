5ef5a9b9496fc0e4ffadec91bad46a9f0ef14139
George <cgeorgesavvas@gmail.com>
2023-01-22 13:08:30 +0000

    backend codesigning mac test

--- backend/build_and_copy.py
+++ backend/build_and_copy.py
@@ -7,7 +7,7 @@ from pathlib import Path
 OS_NAME = platform.system()
 CMDS = {
     "Windows": "pyinstaller --hidden-import=timeago.locales.en src/python/ignite/main.py --noconfirm --uac-admin --windowed --onefile --name=IgniteBackend --icon ../frontend/public/media/desktop_icon/win/icon.ico",
-    "Darwin": "pyinstaller --hidden-import=timeago.locales.en src/python/ignite/main.py --noconfirm --windowed --onefile --codesign-identity=\"Developer ID Application: Georgios Savvas (AJ25U37XWA)\" --osx-entitlements-file=./entitlements.plist --name=IgniteBackend --icon=../frontend/public/media/desktop_icon/mac/icon.icns",
+    "Darwin": "pyinstaller --hidden-import=timeago.locales.en src/python/ignite/main.py --noconfirm --windowed --onefile --codesign-identity={cert_name} --osx-entitlements-file=./entitlements.plist --name=IgniteBackend --icon=../frontend/public/media/desktop_icon/mac/icon.icns",
     "Linux": "pyinstaller --hidden-import=timeago.locales.en src/python/ignite/main.py --noconfirm --windowed --onefile --name=IgniteBackend --icon=../icon.icns"
 }
 
@@ -16,7 +16,10 @@ if not cmd:
     print(f"Could not figure out platform name {OS_NAME}")
     sys.exit()
 print(f"cmd: {cmd}")
-subprocess.run(cmd.split())
+cmd_split = cmd.split()
+for section in cmd_split:
+    section.format(cert_name="Developer ID Application: Georgios Savvas (AJ25U37XWA)")
+subprocess.run(cmd_split)
 current = Path(__file__)
 backend_root = current.parent
 source = backend_root / "dist"
