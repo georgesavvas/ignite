0caa94aa988a906d5033c703bf9e3d4b3b2fd0de
George <cgeorgesavvas@gmail.com>
2023-03-07 04:09:23 +0000

    wip: initial support for dropping scenes

--- backend/src/python/ignite/server/entities/task.py
+++ backend/src/python/ignite/server/entities/task.py
@@ -16,9 +16,10 @@
 from pathlib import Path, PurePath
 
 from ignite.server import utils
-from ignite.server.constants import ANCHORS, DCC_EXTENSIONS
+from ignite.server.constants import ANCHORS
 from ignite.server.entities.asset import Asset
 from ignite.server.entities.directory import Directory
+from ignite.client.utils import get_dcc_scenes
 
 
 class Task(Directory):
@@ -60,7 +61,7 @@ class Task(Directory):
         anchors = kinds.keys()
         extensions = []
         ext_dcc = {}
-        for dcc, exts in DCC_EXTENSIONS.items():
+        for dcc, exts in get_dcc_scenes().items():
             extensions += exts
             for ext in exts:
                 ext_dcc[ext] = dcc
---
+++ frontend/src/views/Explorer/SceneDrop.jsx
@@ -0,0 +1,51 @@
+import { Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import Modal from "../../components/Modal";
+
+import styles from "./SceneDrop.module.css";
+
+
+const SceneDrop = props => {
+  const [selected, setSelected] = useState();
+
+  const handleConfirm = () => props.onClose(selected);
+
+  const style = {
+    border: "solid 2px rgb(0, 150, 0)"
+  };
+
+  return (
+    <Modal
+      maxWidth="xs"
+      open={props.files?.length > 0}
+      onClose={() => props.onClose()}
+      title="Your dropped files contain one or more scenes"
+      buttons={[
+        <Button key="confirm"
+          color="ignite"
+          onClick={handleConfirm}
+          disabled={!selected}
+        >
+          Confirm
+        </Button>
+      ]}
+    >
+      <Typography variant="subtitle1">Would you like to ingest one of them as an actual scene? Any remaining ones will carry over to the New Asset stage to be treated as components</Typography>
+      <div className={styles.container}>
+        {props.files?.map(file =>
+          <div key={file.name}
+            className={styles.file}
+            style={selected === file.name ? style : null}
+            onClick={() => setSelected(file.name)}
+          >
+            <Typography>
+              {file.name}
+            </Typography>
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
+};
+
+export default SceneDrop;
